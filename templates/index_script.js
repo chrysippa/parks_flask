c0b9e3a4f82c0c965fd73193ed8ff0587c641dc1
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2024-05-02 15:34:48 -0500

    Fix dynamic JS rendering

--- templates/index_script.js
+++ templates/index_script.js
@@ -22,7 +22,7 @@ function addMap() {
     var map = L.map("map", {
         center: [46.5, -94.6],
         zoom: 6
-    })
+    });
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: 6,
@@ -30,81 +30,46 @@ function addMap() {
     }).addTo(map);
 
     // Add a marker for each park
-    L.marker([44.84681, -92.79139]).addTo(map)
-    .bindPopup('<a href="">Afton State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
+    var greenIcon = new L.Icon({
+        iconUrl: '{{ url_for('static', filename='img/marker-icon-green.png') }}',
+        shadowUrl: '{{ url_for('static', filename='img/marker-shadow.png') }}',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
-    L.marker([46.17349, -92.84968]).addTo(map)
-    .bindPopup('<a href="">Banning State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
+    var yellowIcon = new L.Icon({
+        iconUrl: '{{ url_for('static', filename='img/marker-icon-gold.png') }}',
+        shadowUrl: '{{ url_for('static', filename='img/marker-shadow.png') }}',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
-    L.marker([44.52373, -92.34214]).addTo(map)
-    .bindPopup('<a href="">Frontenac State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
+    var redIcon = new L.Icon({
+        iconUrl: '{{ url_for('static', filename='img/marker-icon-red.png') }}',
+        shadowUrl: '{{ url_for('static', filename='img/marker-shadow.png') }}',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
-    L.marker([47.14092, -91.46971]).addTo(map)
-    .bindPopup('<a href="">Gooseberry Falls State Park</a>')
+    {% for p in parks %}
+    L.marker([{{ p['latitude'] }}, {{ p['longitude'] }}], {icon: 
+        {% if p['level'] == 'bad' %}
+        redIcon
+        {% elif p['level'] == 'warning' %}
+        yellowIcon
+        {% else %}
+        greenIcon
+        {% endif %}
+    }).addTo(map)
+    .bindPopup('<a href="{{ url_for('park', park_id=p['park_id']) }}">{{ p['name'] }}</a>')
     .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([48.00055, -89.59312]).addTo(map)
-    .bindPopup('<a href="">Grand Portage State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([43.93956, -91.40834]).addTo(map)
-    .bindPopup('<a href="">Great River Bluffs State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([45.39366, -92.66899]).addTo(map)
-    .bindPopup('<a href="">Interstate State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([46.65433, -92.37127]).addTo(map)
-    .bindPopup('<a href="">Jay Cooke State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([45.31376, -93.94101]).addTo(map)
-    .bindPopup('<a href="">Lake Maria State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([46.13559, -93.72518]).addTo(map)
-    .bindPopup('<a href="">Mille Lacs Kathio State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([43.63729, -93.30918]).addTo(map)
-    .bindPopup('<a href="">Myre-Big Island State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([44.34225, -93.10519]).addTo(map)
-    .bindPopup('<a href="">Nerstrand Big Woods State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([45.94622, -92.60659]).addTo(map)
-    .bindPopup('<a href="">St. Croix State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([47.33848, -91.19606]).addTo(map)
-    .bindPopup('<a href="">Tettegouche State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([44.06227, -92.04486]).addTo(map)
-    .bindPopup('<a href="">Whitewater State Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([45.22504, -92.76562]).addTo(map)
-    .bindPopup("<a href=''>William O'Brien State Park</a>")
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([44.78643, -93.12888]).addTo(map)
-    .bindPopup('<a href="">Lebanon Hills Regional Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([44.76568, -92.93711]).addTo(map)
-    .bindPopup('<a href="">Spring Lake Park Reserve</a>')
-    .on('click', (e) => {e.target.openPopup()});
-
-    L.marker([44.72505, -92.84999]).addTo(map)
-    .bindPopup('<a href="">Vermillion Falls Park</a>')
-    .on('click', (e) => {e.target.openPopup()});
+    {% endfor %}
 };
 
 addEventListener("load", addMap);
